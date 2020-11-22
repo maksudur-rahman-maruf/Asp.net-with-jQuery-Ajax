8548d6bd5b50e28e9ff61b8843a1dcd4c33e757c
maruf-mbstu <maruf.mbstu@gmail.com>
2020-11-22 21:42:48 +0600

    first commit

---
+++ mvcCrudDbFirst/mvcCrudDbFirst/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using mvcCrudDbFirst.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcCrudDbFirst.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+
+        db_testEntities dbObj = new db_testEntities();
+        public ActionResult Student()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddStudent(tbl_Student model)
+        {
+            if(ModelState.IsValid)
+            {
+                tbl_Student obj = new tbl_Student();
+                obj.Name = model.Name;
+                obj.Fname = model.Fname;
+                obj.Email = model.Email;
+                obj.Mobile = model.Mobile;
+                obj.Description = model.Description;
+
+                dbObj.tbl_Student.Add(obj);
+                dbObj.SaveChanges();
+            }
+            ModelState.Clear();
+
+            return View("Student");
+        }
+
+        public ActionResult StudentList()
+        {
+            var res = dbObj.tbl_Student.ToList();
+            return View(res);
+        }
+
+    }
+}
---
+++ mvcCrudDbFirst/mvcCrudDbFirst/Scripts/Scripts.js
@@ -0,0 +1,17 @@
+﻿function jQueryAjaxPost(form)
+{
+    $.validator.unobtrusive.parse(form);
+    if ($(form).valid())
+    {
+        $.ajax({
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            success: function (response) {
+                console.log(response);
+            }
+
+        });
+    }
+    return false;
+}
